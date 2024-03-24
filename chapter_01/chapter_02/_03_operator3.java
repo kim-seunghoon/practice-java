e0c9585618df9c5d79a212c4f6c8342e6638fa3e
kim-seunghoon <164722492+kim-seunghoon@users.noreply.github.com>
2024-03-24 16:58:37 +0900

    Delete chapter_01/chapter_02 directory

--- chapter_01/chapter_02/_03_operator3.java
+++
@@ -1,9 +0,0 @@
-package chapter_02;
-
-public class _03_operator3 {
-    public static void main(String[] args) {
-        //비교 연산자
-        System.out.println(5 == 5);
-        System.out.println(5 != 5);
-    }
-}
